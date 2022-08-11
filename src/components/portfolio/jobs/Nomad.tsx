6f6a932d6f29f37673d0702eb198a4d63a83fba2
Leticia Godoy <let.godoy@live.com>
2022-08-10 21:36:17 -0300

    ajuste texto

--- src/components/portfolio/jobs/Nomad.tsx
+++ src/components/portfolio/jobs/Nomad.tsx
@@ -8,7 +8,7 @@ export const Nomad = () => {
         name="Nomad"
         client='Nomad'
         year='2022'
-        description={<p>A Nomad é uma empresa de fintech que possibilita soluções financeiras de um jeito fácil e prático, propondo novas formas de administrar seu dinheiro e diversificar seus investimentos. Com ela, você pode ter uma conta em dólar 100% digital, explorando possibilidades de compra, economia e poupança enquanto expande suas fronteiras para universos nunca antes imaginados. A Duee Brasil foi responsável pela roteirização e produção dos vídeos explicativos.</p>}
+        description={<p>A Nomad é uma empresa de fintech que possibilita soluções financeiras de um jeito fácil e prático, propondo novas formas de administrar seu dinheiro e diversificar seus investimentos. Com ela, você pode ter uma conta em dólar 100% digital, explorando possibilidades de compra, economia e poupança enquanto expande suas fronteiras para universos nunca antes imaginados. A Duee Brasil foi responsável pela roteirização, criação de campanhas publicitárias e produção dos vídeos.</p>}
         header="https://portfolio.duee.com.br/wp-content/uploads/2022/08/header-1.png"
     >
         <div className="col-xs-12">
