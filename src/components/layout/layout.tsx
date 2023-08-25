22dffaa3b8a59a86d10953d250ff49462834afa8
Leticia Godoy <let.godoy@live.com>
2023-08-25 18:16:21 -0300

    removendo firebase?

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -7,7 +7,6 @@ import GlobalStyled from "./globalStyle"
 import Navbar from "@components/navbar/navbar"
 import Wpp from "@images/whatsapp.svg"
 
-// import "./init-firebase"
 
 const Wppbutton = styled.a`
   position: fixed;
