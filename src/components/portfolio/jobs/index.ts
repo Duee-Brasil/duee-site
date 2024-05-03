b2882aa5175f2aa9dceb5b23e91c624596436274
Leticia Godoy <let.godoy@live.com>
2024-05-03 18:29:28 -0300

    add natal stbx 23

--- src/components/portfolio/jobs/index.ts
+++ src/components/portfolio/jobs/index.ts
@@ -122,8 +122,15 @@ import { AngosturaIG } from "./angostura-ig"
 import { StarbucksHalloween } from "./stbx-halloween"
 import { StarbucksPride } from "./stbx-pride"
 import { StarbucksWinter23 } from "./stbx-winter23"
+import { StarbucksNatal23 } from "./stbx-natal23"
 
 export const JobsList = [
+  {
+    thumbnail:
+      "https://firebasestorage.googleapis.com/v0/b/duee-site.appspot.com/o/portifolio%2Fjobs%2Fstbx-natal23%2Fcapa.png?alt=media&token=c6514583-0949-403a-93d6-6f3b9f2525dc",
+    title: "Natal Starbucks 2023",
+    job: StarbucksNatal23,
+  },
   {
     thumbnail:
       "https://firebasestorage.googleapis.com/v0/b/duee-site.appspot.com/o/portifolio%2Fjobs%2Fstbx-winter23%2Fcapa.png?alt=media&token=f0b2547b-b9bc-471f-b42d-83230eeffa3f",
---
+++ src/components/portfolio/jobs/stbx-natal23.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { CaseBase } from '../caseBase'
+
+export const StarbucksNatal23 = () => {
+    return <CaseBase
+        backgroundColor='#fff'
+        color='black'
+        name="Natal Starbucks 2023"
+        client='Starbucks'
+        year='2023'
+        description={<p>A esperada temporada de Natal na
+            Starbucks trouxe a doçura específica
+            dessa época tão aguardada, com muito
+            chocolate e sabores intensos. A DUEE
+            Brasil desenvolveu um papel importante
+            na criação de uma atmosfera das
+            lojas, focando em cada detalhe para
+            aproximar os clientes de todo esse
+            universo natalino com Starbucks.
+        </p>}
+        header="https://firebasestorage.googleapis.com/v0/b/duee-site.appspot.com/o/portifolio%2Fjobs%2Fstbx-natal23%2Fheadder.png?alt=media&token=d467f451-db0e-4fd9-b7da-39d74735e16b"
+    >
+        <div className="col-xs-12 col-sm-6">
+            <img src="https://firebasestorage.googleapis.com/v0/b/duee-site.appspot.com/o/portifolio%2Fjobs%2Fstbx-natal23%2FImagem%2001.png?alt=media&token=6d5e9c60-3482-40b7-ae49-6b1bd706116c" alt="" />
+        </div>
+        <div className="col-xs-12 col-sm-3">
+            <img src="https://firebasestorage.googleapis.com/v0/b/duee-site.appspot.com/o/portifolio%2Fjobs%2Fstbx-natal23%2FImagem%2002.png?alt=media&token=39afcb4b-fd29-47a9-b844-5ee7cb0599b6" alt="" />
+        </div>
+        <div className="col-xs-12 col-sm-3">
+            <img src="https://firebasestorage.googleapis.com/v0/b/duee-site.appspot.com/o/portifolio%2Fjobs%2Fstbx-natal23%2FImagem%2003.png?alt=media&token=b9a8056d-a927-4fee-a1ca-d09a876680bf" alt="" />
+            <img src="https://firebasestorage.googleapis.com/v0/b/duee-site.appspot.com/o/portifolio%2Fjobs%2Fstbx-natal23%2FImagem%2005.png?alt=media&token=815c3bdd-4e1e-4aba-846d-3a802daf6fe7" alt="" />
+        </div>
+        <div className="col-xs-12">
+            <img src="https://firebasestorage.googleapis.com/v0/b/duee-site.appspot.com/o/portifolio%2Fjobs%2Fstbx-natal23%2FImagem%2004.png?alt=media&token=a10a8be0-f4d2-4473-96ee-8ae31e475c23" alt="" />
+        </div>
+        <div className="col-xs-12 col-sm-6 col-md-4">
+            <img src="https://firebasestorage.googleapis.com/v0/b/duee-site.appspot.com/o/portifolio%2Fjobs%2Fstbx-natal23%2FImagem%2006.png?alt=media&token=51bc6fcb-1b40-4278-854c-bd94225ce8b0" alt="" />
+        </div>
+    </CaseBase>
+
+}
